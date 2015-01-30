9681a9141ac254b5da535a48a27b30adfd695775
dbgustlr92 <dbgustlr92@naver.com>
2015-01-30 19:43:15 +0900

    버튼 추가 , 돌맹이 추가 , 버튼 눌렸을때 적용

--- Assets/3_Script/PlayerScript.cs
+++ Assets/3_Script/PlayerScript.cs
@@ -8,13 +8,14 @@ public class PlayerScript : MonoBehaviour {
     // 설정값
     const float jump_speed = 0.05f;  // 점프속도
     const float jump_accell = 0.0025f; // 점프가속
-    const float y_base = -0.25f;      // 캐릭터가 서있는 기준점
+    const float y_base = -0.11f;      // 캐릭터가 서있는 기준점
     bool jump2 = true;
 
     public int _hp = 2000;
     public GameObject _DamageEff;
     public UIFilledSprite _GuageBarWidget;  //체력 게이지
     public UIFilledSprite _PowerGauge;   //파워게이지
+    public GameObject Garbage_Set;
     public GameObject Stone_Set;
     public GameObject Fork_Set;
    
@@ -28,9 +29,10 @@ public class PlayerScript : MonoBehaviour {
     public int score;
     public UILabel ScorePanel;
 
-
+    
     void Start()
     {
+        //y = Player.transform.localPosition.y;
         y = y_base;
         
         
@@ -38,6 +40,9 @@ public class PlayerScript : MonoBehaviour {
     
     void Update()
     {
+        
+        UISprite[] child = GameObject.Find("1_BGObj").GetComponentsInChildren<UISprite>();
+        
         /////////파워게이지 자동으로 오르락 내리락 부분
         if (_power > 0 && Powerswitch)
         {
@@ -63,21 +68,31 @@ public class PlayerScript : MonoBehaviour {
             Application.LoadLevel("3");
         }
         if (Input.GetKeyUp(KeyCode.Q))          //q눌럿을때 포크포크
-        {
+        { 
             var attack_fork = Instantiate(Fork_Set, Vector3.zero, Quaternion.identity) as GameObject;
             attack_fork.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
             attack_fork.transform.localPosition = new Vector3(-1.5f, Player.transform.position.y, 0);
             attack_fork.transform.parent = _parent;
             attack_fork.gameObject.GetComponent<MoveFood>().power = _power;
+            
         }
+       
+        
         if (Input.GetKeyUp(KeyCode.W))        //w 눌렀을때 무기발사 (쓔레기통)
         {
+            var attack_GarbageCan = Instantiate(Garbage_Set, Vector3.zero, Quaternion.identity) as GameObject;
+            attack_GarbageCan.transform.localScale = new Vector3(1.5f, 2f, 2f);
+            attack_GarbageCan.transform.localPosition = new Vector3(-2f, Player.transform.position.y, 0);
+            attack_GarbageCan.transform.parent = _parent;
+            attack_GarbageCan.gameObject.GetComponent<MoveFood>().power = _power * 5;
+            
+        }
+        if (Input.GetKeyUp(KeyCode.E))
+        {
             var attack_Stone = Instantiate(Stone_Set, Vector3.zero, Quaternion.identity) as GameObject;
-            attack_Stone.transform.localScale = new Vector3(1.5f, 2f, 2f);
             attack_Stone.transform.localPosition = new Vector3(-2f, Player.transform.position.y, 0);
             attack_Stone.transform.parent = _parent;
             attack_Stone.gameObject.GetComponent<MoveFood>().power = _power * 5;
-            
         }
         
         if (Input.GetKeyDown(KeyCode.DownArrow))
@@ -94,7 +109,14 @@ public class PlayerScript : MonoBehaviour {
         JumpProcess();
         if (Input.GetKeyDown(KeyCode.Space)/*Input.touchCount>0*/)
         {
+            child[3].GetComponent<UISprite>().spriteName = "jump&slide_pushed";
             DoJump();
+        }
+
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            child[3].GetComponent<UISprite>().spriteName = "점프&슬라이드";
+            
         }
         // y값을 gameObject에 적용하세요.
         Vector3 pos = gameObject.transform.position;
@@ -104,10 +126,6 @@ public class PlayerScript : MonoBehaviour {
         _hp--;
         _GuageBarWidget.fillAmount = _hp * 0.0005f;
 
-        /*var _Eff1 = Instantiate(_DamageEff, transform.localPosition, Quaternion.identity) as GameObject;
-        _Eff1.transform.parent = transform;
-        _Eff1.transform.localPosition = Vector3.zero;
-        _Eff1.transform.localScale = new Vector3(1, 1, 1);*/
 
         ScorePanel.text = score.ToString();
 
--- Assets/3_Script/Player_R.cs
+++ Assets/3_Script/Player_R.cs
@@ -7,6 +7,10 @@ public class Player_R : MonoBehaviour {
     public GameObject _DamageEff;
     public UIFilledSprite _GuageBarWidget;
     public float power;
+
+    public float sumTime = 0;
+    bool isLoading = false;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -14,7 +18,22 @@ public class Player_R : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        sumTime += Time.deltaTime;
+        UISprite[] ChildTs = GetComponentsInChildren<UISprite>();
+        if (sumTime >= 1)           //1초마다 표정 바뀜
+        {
+          
+            if (ChildTs[2].GetComponent<UISprite>().spriteName.Equals("05_R_HEAD_00"))
+            {
+                ChildTs[2].GetComponent<UISprite>().spriteName = "05_R_HEAD_01";
+                sumTime = 0;
+            }
+            else
+            {
+                ChildTs[2].GetComponent<UISprite>().spriteName = "05_R_HEAD_00";
+                sumTime = 0;
+            }
+        }
 	}
 
     void OnTriggerEnter(Collider other)
@@ -32,6 +51,34 @@ public class Player_R : MonoBehaviour {
             
             Destroy(other.gameObject);
             
+        }
+
+        if (other.name.Equals("GarbageCan"))      //무기타입별로 체력 다르게 
+        {
+            _hp -= (float)0.05 * other.GetComponentInParent<MoveFood>().power;     //부모의 값을 받아야 하므로 부모로 이동해서 값을 받아옵니다. 
+
+            _GuageBarWidget.fillAmount = _hp * 0.01f;
+            var _Eff1 = Instantiate(_DamageEff, transform.localPosition, Quaternion.identity) as GameObject;
+            _Eff1.transform.parent = transform;
+            _Eff1.transform.localPosition = Vector3.zero;
+            _Eff1.transform.localScale = new Vector3(1, 1, 1);
+
+            Destroy(other.gameObject);
+
+        }
+
+        if (other.name.Equals("Stone"))      //무기타입별로 체력 다르게 
+        {
+            _hp -= (float)0.03 * other.GetComponentInParent<MoveFood>().power;     //부모의 값을 받아야 하므로 부모로 이동해서 값을 받아옵니다. 
+
+            _GuageBarWidget.fillAmount = _hp * 0.01f;
+            var _Eff1 = Instantiate(_DamageEff, transform.localPosition, Quaternion.identity) as GameObject;
+            _Eff1.transform.parent = transform;
+            _Eff1.transform.localPosition = Vector3.zero;
+            _Eff1.transform.localScale = new Vector3(1, 1, 1);
+
+            Destroy(other.gameObject);
+
         }
     }
 }
